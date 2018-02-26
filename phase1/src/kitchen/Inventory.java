153032d502f69d7d8e7b0fbc00885602d7cfb8a0
tinally <lunyu.li@mail.utoronto.ca>
2018-02-26 14:00:24 -0500

    Added an Inventory class for recording the use of all ingredients.

--- phase1/src/kitchen/Inventory.java
+++ phase1/src/kitchen/Inventory.java
@@ -3,4 +3,37 @@ package kitchen;
 import java.util.HashMap;
 
 public class Inventory {
+    private HashMap<Ingredient, Integer> inventory;
+    private HashMap<Ingredient, Integer> threshold;
+
+    public Inventory() {
+        inventory = new HashMap<>();
+        threshold = new HashMap<>();
+    }
+
+    public int getLeftOver(Ingredient ingredient) {
+        return inventory.get(ingredient);
+    }
+
+    public HashMap<Ingredient, Integer> getInventory() {
+        return inventory;
+    }
+
+    public HashMap<Ingredient, Integer> getThreshold() {
+        return threshold;
+    }
+
+    public boolean reOrder(Ingredient ingredient) {
+        int num = inventory.get(ingredient);
+        int limit = threshold.get(ingredient);
+        return num < limit;     // TODO: update requests.txt
+    }
+
+    public String toString() {
+        String s = "{";
+        for (Ingredient ingredient : inventory.keySet()) {
+            s += "( " + ingredient.getName() + ", " + inventory.get(ingredient) + " ), ";
+        }
+        return s.substring(0, s.length() - 2) + "}";
+    }
 }
