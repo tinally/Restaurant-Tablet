3ee7303e9bebf0c7e59a3efe2d45e27d9da189f4
tinally <lunyu.li@mail.utoronto.ca>
2018-02-26 13:46:24 -0500

    Added an Ingredient class for all ingredients of the menu items.

--- phase1/src/kitchen/Ingredient.java
+++ phase1/src/kitchen/Ingredient.java
@@ -1,11 +1,19 @@
 package kitchen;
 
 public class Ingredient {
-    public String name;
-    public Double cost;
+    private String name;
+    private Double cost;
 
     public Ingredient(String name, Double cost) {
         this.name = name;
         this.cost = cost;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getCost() {
+        return this.cost;
     }
 }
