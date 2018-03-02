864265e8bc19e444f5c029ee26bf841d351772f8
Eric Lindau <eric@lindautech.com>
2018-03-02 02:07:02 -0500

    Add PaymentManager, refactor Bill/BillPrinterService

--- phase1/src/payment/Bill.java
+++ phase1/src/payment/Bill.java
@@ -1,41 +1,39 @@
 package payment;
 
+import restaurant.MenuItem;
+
 import java.util.ArrayList;
 
 /**
  * A Bill represents a collection of Bill Items that must be paid for
- * by a customer.
+ * by a Table.
  */
 public class Bill {
 
     /**
-     * A collection of BillItems that are to be paid with this Bill.
+     * The MenuItems that are to be paid for with this Bill.
      */
-    @Deprecated
-    private ArrayList<BillItem> billItems;
-
-    // TODO: Javadoc / Implement once MenuItems implemented
-//    private ArrayList<MenuItem> menuItems;
+    private ArrayList<MenuItem> menuItems;
 
     /**
-     * The number of ways to split this Bill among customers at a Table.
+     * The number of ways to split this Bill among customers at the Table.
      */
     private int splits;
 
+    /**
+     * Bill constructor.
+     */
     public Bill() {
-        // TODO: Implement once MenuItems implemented
-//        menuItems = new ArrayList<>();
+        menuItems = new ArrayList<>();
         splits = 1;
     }
 
-    // TODO: Javadoc / Implement once MenuItems implemented
-//    public void addMenuItem(MenuItem menuItem) {
-//
-//    }
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
 
-    // TODO: Javadoc / Implement once MenuItems implemented
-//    public MenuItem[] getMenuItems() {
-//
-//    }
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
 
 }
