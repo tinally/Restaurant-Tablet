9e56d3086076aa8125234029fc65521485e591dd
Nasir Hemed <nasirhemed@ymail.com>
2018-03-02 18:00:52 -0500

    Add Javadoc for table class

--- phase1/src/restaurant/Table.java
+++ phase1/src/restaurant/Table.java
@@ -1,8 +1,17 @@
 package restaurant;
 
 public class Table {
+    /**
+     * The table number
+     */
     private int tableNumber;
+    /**
+     * The number of customers on that table
+     */
     private int numCustomers;
+    /**
+     * The Server serving this table
+     */
 //    private Server server;
 
     public Table(int tableNumber, int numCustomers){
@@ -11,19 +20,43 @@ public class Table {
 
     }
 
+    /**
+     *
+     * @return the number of customers on this table
+     */
     public int getNumCustomers() {
         return numCustomers;
     }
 
+    /**
+     *
+     * @return the tableNumber
+     */
     public int getTableNumber() {
         return tableNumber;
     }
 
+    /**
+     *
+     * @param numCustomers the number of customers to be set.
+     */
     public void setNumCustomers(int numCustomers) {
         this.numCustomers = numCustomers;
     }
 
+    /**
+     *
+     * @param tableNumber the tableNumber to be set
+     */
     public void setTableNumber(int tableNumber) {
         this.tableNumber = tableNumber;
     }
+
+//    public Server getServer() {
+//        return server;
+//    }
+//
+//    public void setServer(Server server) {
+//        this.server = server;
+//    }
 }
