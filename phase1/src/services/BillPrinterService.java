581d678edd70dd0a858ad6af771d01ff854db481
Eric Lindau <eric@lindautech.com>
2018-03-05 13:12:55 -0500

    Remove ID from BillPrinterService

--- phase1/src/services/BillPrinterService.java
+++ phase1/src/services/BillPrinterService.java
@@ -5,7 +5,7 @@ import restaurant.OrderItem;
 import services.framework.Service;
 import services.framework.ServiceConstructor;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 /**
  * This Service prints one or more Bills according to specification.
@@ -15,28 +15,24 @@ public class BillPrinterService extends Service {
     /**
      * The Bills to be printed.
      */
-    private HashMap<Integer, Bill> bills;
+//    private HashMap<Integer, Bill> bills;
+
+    private ArrayList<Bill> bills;
 
     /**
      * BillPrinterService constructor.
      */
     @ServiceConstructor
     public BillPrinterService() {
-        bills = new HashMap<>();
+        bills = new ArrayList<>();
     }
 
     /**
      * Add a new Bill to be printed.
-     * @param id the ID with which this Bill will be associated.
-     * @return false iff the given ID is already associated with a Bill.
+     * @param bill the Bill to be added.
      */
-    public boolean addBill(Integer id, Bill bill) {
-        if (bills.containsKey(id)) {
-            return false;
-        } else {
-            bills.put(id, bill);
-            return true;
-        }
+    public void addBill(Bill bill) {
+        bills.add(bill);
     }
 
     // TODO: Decide how to format this
@@ -63,33 +59,34 @@ public class BillPrinterService extends Service {
      */
     public String printBills() {
         StringBuilder accumulator = new StringBuilder();
-        for (Integer billId : bills.keySet()) {
-            accumulator.append(printBill(billId));
+        for (Bill bill : bills) {
+            accumulator.append(printBill(bill));
         }
+
         return accumulator.toString();
     }
 
-    /**
-     * @param id the ID of the Bill to print.
-     * @return a String representation of the Bill with the specified ID.
-     */
-    public String printBill(Integer id) {
-        StringBuilder accumulator = new StringBuilder();
-        Bill bill = bills.get(id);
-        // First, print out the ID of the bill being printed
-        accumulator.append(id);
-        accumulator.append(System.lineSeparator());
-        // Then, print each menu item of the bill (indent included) in format
-        // ITEM:PRICE
-        for (OrderItem orderItem : bill.getOrderItems()) {
-            accumulator.append('\t');
-            accumulator.append(orderItem.toString());
-            accumulator.append(':');
-            // TODO: Change to getPrice() - discount should always be applied
-            accumulator.append(orderItem.getMenuItem().getOriginalPrice());
-            accumulator.append(System.lineSeparator());
-        }
-        return accumulator.toString();
-    }
+//    /**
+//     * @param id the ID of the Bill to print.
+//     * @return a String representation of the Bill with the specified ID.
+//     */
+//    public String printBill(Integer id) {
+//        StringBuilder accumulator = new StringBuilder();
+//        Bill bill = bills.get(id);
+//        // First, print out the ID of the bill being printed
+//        accumulator.append(id);
+//        accumulator.append(System.lineSeparator());
+//        // Then, print each menu item of the bill (indent included) in format
+//        // ITEM:PRICE
+//        for (OrderItem orderItem : bill.getOrderItems()) {
+//            accumulator.append('\t');
+//            accumulator.append(orderItem.toString());
+//            accumulator.append(':');
+//            // TODO: Change to getPrice() - discount should always be applied
+//            accumulator.append(orderItem.getMenuItem().getOriginalPrice());
+//            accumulator.append(System.lineSeparator());
+//        }
+//        return accumulator.toString();
+//    }
 
 }
