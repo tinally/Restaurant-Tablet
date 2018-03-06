5cb2f4440fd5215215e61b0635e54485669a6873
Nasir Hemed <nasirhemed@ymail.com>
2018-03-06 14:39:57 -0500

    Use inventory's addToInventory method

--- phase1/src/kitchen/Receiver.java
+++ phase1/src/kitchen/Receiver.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public class Receiver {
     private Inventory inventory;
-    //TODO: Scan each item
 
     public void scanItem(Ingredient ingredient){
-        inventory.addIngredient(ingredient, 20); //20 is the default value.
-        //TODO: add an event?
+        inventory.addToInventory(ingredient, 20); //20 is the default value.
     }
 
     public void ScanItems(List<Ingredient> ingredients){
