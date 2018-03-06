4b02d8e1ca134d181c17d86dfd36070cbf975576
Eric Lindau <eric@lindautech.com>
2018-03-06 14:10:22 -0500

    Fix indendation

--- phase1/src/payment/Bill.java
+++ phase1/src/payment/Bill.java
@@ -12,34 +12,34 @@ import java.util.ArrayList;
  */
 public class Bill {
 
-    /**
-     * The order items that are outstanding (to be paid for) with this bill.
-     */
-    private ArrayList<OrderItem> orderItems;
+  /**
+   * The order items that are outstanding (to be paid for) with this bill.
+   */
+  private ArrayList<OrderItem> orderItems;
 
-    /**
-     * The table to which this bill belongs.
-     */
-    private Table table;
+  /**
+   * The table to which this bill belongs.
+   */
+  private Table table;
 
-    /**
-     * Constructs a new Bill for the specified table.
-     *
-     * @param table the table to which this bill belongs.
-     */
-    public Bill(Table table) {
-        orderItems = new ArrayList<>();
-        this.table = table;
-    }
+  /**
+   * Constructs a new Bill for the specified table.
+   *
+   * @param table the table to which this bill belongs.
+   */
+  public Bill(Table table) {
+    orderItems = new ArrayList<>();
+    this.table = table;
+  }
 
-    /**
-     * Returns the order items associated with this bill.
-     *
-     * @return the order items associated with this bill.
-     */
-    public ArrayList<OrderItem> getOrderItems() {
-        return orderItems;
-    }
+  /**
+   * Returns the order items associated with this bill.
+   *
+   * @return the order items associated with this bill.
+   */
+  public ArrayList<OrderItem> getOrderItems() {
+    return orderItems;
+  }
 
   /**
    * Returns the table associated with this bill.
@@ -47,16 +47,16 @@ public class Bill {
    * @return the table associated with this bill.
    */
   public Table getTable() {
-      return table;
-    }
+    return table;
+  }
 
-    /**
-     * Adds an order to this bill.
-     *
-     * @param order the order whose items will be added to this bill.
-     */
-    public void addOrder(Order order) {
-        orderItems.addAll(order.getItems());
-    }
+  /**
+   * Adds an order to this bill.
+   *
+   * @param order the order whose items will be added to this bill.
+   */
+  public void addOrder(Order order) {
+    orderItems.addAll(order.getItems());
+  }
 
 }
