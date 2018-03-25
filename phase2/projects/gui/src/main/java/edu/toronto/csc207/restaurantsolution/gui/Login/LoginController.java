0300a08eca80e1a62ff2614d567a850d3f8cab6a
Nasir Hemed <nasirhemed@ymail.com>
2018-03-24 22:34:32 -0400

    Add transition to different scene when logged in

--- phase2/projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Login/LoginController.java
+++ phase2/projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Login/LoginController.java
@@ -3,9 +3,17 @@ package edu.toronto.csc207.restaurantsolution.gui.Login;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import edu.toronto.csc207.restaurantsolution.gui.Chef.ChefGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class LoginController {
 
@@ -18,12 +26,42 @@ public class LoginController {
     @FXML
     private JFXButton login;
 
-    @FXML
-    void validateUser(ActionEvent event) {
+    private void setScene(ActionEvent event, URL url) throws IOException{
+        System.out.println(url);
+        Parent newRoot = FXMLLoader.load(url);
+        Scene scene1 = new Scene(newRoot);
+        Stage window = (Stage)(((Node)event.getSource()).getScene()).getWindow();
+        window.setScene(scene1);
+        window.setFullScreen(true);
+        window.show();
+    }
 
-        if (username.getText().equals("CSC207") && password.getText().equals("phase2")){
+    @FXML
+    void validateUser(ActionEvent event) throws IOException {
+
+        String id = username.getText();
+        String pass = password.getText();
+        if (id.equals("Chef") && pass.equals("chef")){
             System.out.println("Logged In");
-            }
+            URL url = new File("projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Chef/Chef.fxml").toURL();
+            setScene(event, url);
+        }
+        else if(id.equals("Server") && pass.equals("server")){
+            URL url = new File("projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Server/Server.fxml").toURL();
+            setScene(event, url);
+        }
+        else if(id.equals("Receiver") && pass.equals("receiver")){
+            URL url = new File("projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Receiver/Receiver.fxml").toURL();
+            setScene(event, url);
+        }
+        else if(id.equals("Manager") && pass.equals("manager")){
+            URL url = new File("projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Manager/Manager.fxml").toURL();
+            setScene(event, url);
+        }
+        else if(id.equals("Cashier") && pass.equals("cashier")){
+            URL url = new File("projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Cashier/Cashier.fxml").toURL();
+            setScene(event, url);
+        }
         else{
             System.out.println("Try Again");
         }
