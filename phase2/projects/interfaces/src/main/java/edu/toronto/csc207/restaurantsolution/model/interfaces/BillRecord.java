462900b13715d265f99fad4c873318818c57ff6a
Lun Yu Li <lunyu.li@mail.utoronto.ca>
2018-03-26 15:09:29 -0400

    Interface comments.

--- phase2/projects/interfaces/src/main/java/edu/toronto/csc207/restaurantsolution/model/interfaces/BillRecord.java
+++ phase2/projects/interfaces/src/main/java/edu/toronto/csc207/restaurantsolution/model/interfaces/BillRecord.java
@@ -2,16 +2,59 @@ package edu.toronto.csc207.restaurantsolution.model.interfaces;
 
 
 import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Represents a bill record for a table.
+ */
 public interface BillRecord {
-  UUID getBillID();
-  List<Order> getBilledOrders();
-  Double getChargedSubtotal();
-  Double getChargedTax();
-  Double getChargedGratuity();
-  Double getPaidAmount();
-  Instant getBilledDate();
+    /**
+     * Returns an unique ID for a bill.
+     *
+     * @return an unique ID for a bill.
+     */
+    UUID getBillID();
+
+    /**
+     * Returns List of Orders that are billed.
+     *
+     * @return List of Orders that are billed.
+     */
+    List<Order> getBilledOrders();
+
+    /**
+     * Returns the subtotal of the bill.
+     *
+     * @return the subtotal of the bill.
+     */
+    Double getChargedSubtotal();
+
+    /**
+     * Returns the tax included for the bill.
+     *
+     * @return the tax included for the bill.
+     */
+    Double getChargedTax();
+
+    /**
+     * Returns the gratuity included for the bill.
+     *
+     * @return the gratuity included for the bill.
+     */
+    Double getChargedGratuity();
+
+    /**
+     * Returns the total amount paid.
+     *
+     * @return the total amount paid.
+     */
+    Double getPaidAmount();
+
+    /**
+     * Returns the date of the bill.
+     *
+     * @return the date of the bill.
+     */
+    Instant getBilledDate();
 }
--- phase2/projects/interfaces/src/main/java/edu/toronto/csc207/restaurantsolution/model/interfaces/Ingredient.java
+++ phase2/projects/interfaces/src/main/java/edu/toronto/csc207/restaurantsolution/model/interfaces/Ingredient.java
@@ -2,13 +2,53 @@ package edu.toronto.csc207.restaurantsolution.model.interfaces;
 
 import java.io.Serializable;
 
+/**
+ * Represents an Ingredient in the stock.
+ */
 public interface Ingredient extends Serializable {
-  String getName();
-  Double getCost();
-  Double getPricing();
-  Integer getReorderThreshold();
-  Integer getDefaultReorderAmount();
-  default boolean equals(Ingredient i) {
-    return this.getName().equals(i.getName());
-  }
+    /**
+     * Returns the name of the Ingredient.
+     *
+     * @return the name of the Ingredient.
+     */
+    String getName();
+
+    /**
+     * Returns the cost of the Ingredient.
+     *
+     * @return the cost of the Ingredient.
+     */
+    Double getCost();
+
+    /**
+     * Returns the pricing of a single unit of Ingredient.
+     *
+     * @return the pricing of a single unit of Ingredient.
+     */
+    Double getPricing();
+
+    /**
+     * Returns the threshold for reordering.
+     * If the amount of Ingredient in stock is less than the threshold, we request for reorder.
+     *
+     * @return the threshold for reordering.
+     */
+    Integer getReorderThreshold();
+
+    /**
+     * Returns the default amount of Ingredient for reordering.
+     *
+     * @return the default amount of Ingredient for reordering.
+     */
+    Integer getDefaultReorderAmount();
+
+    /**
+     * Checks whether ingredient is the same as this.
+     *
+     * @param ingredient the Ingredient to be compared with this
+     * @return true if ingredient and this are the same; false otherwise.
+     */
+    default boolean equals(Ingredient ingredient) {
+        return this.getName().equals(ingredient.getName());
+    }
 }
