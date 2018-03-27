3b825196a937c69bc6a85e0fc12e1a9af87a8bd4
Lun Yu Li <lunyu.li@mail.utoronto.ca>
2018-03-26 23:16:49 -0400

    ServerController.java edits.

--- phase2/projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Server/ServerController.java
+++ phase2/projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/Server/ServerController.java
@@ -14,7 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 import java.util.UUID;
 import java.util.Map;
