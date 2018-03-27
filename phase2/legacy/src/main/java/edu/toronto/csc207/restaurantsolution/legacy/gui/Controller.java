a02246fd0cac865a3d036cbdf0e1051724eb3c18
Lun Yu Li <lunyu.li@mail.utoronto.ca>
2018-03-27 04:34:22 -0400

    Deleted duplicate and unnecessary code.

--- phase2/legacy/src/main/java/edu/toronto/csc207/restaurantsolution/legacy/gui/Controller.java
+++
@@ -1,9 +0,0 @@
-package edu.toronto.csc207.restaurantsolution.legacy.gui;
-
-import javafx.event.ActionEvent;
-
-public class Controller {
-  public void sayHelloWorld(ActionEvent actionEvent) {
-    System.out.println("Hello World!");
-  }
-}
