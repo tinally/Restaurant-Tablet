90cc44ac81fa9ab6045166306c7a9efd743f0e02
Nasir Hemed <nasirhemed@ymail.com>
2018-03-26 21:51:52 -0400

    Add components to CreateAccountController and its FXML file

--- phase2/projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/newAccount/createAccountController.java
+++ phase2/projects/gui/src/main/java/edu/toronto/csc207/restaurantsolution/gui/newAccount/createAccountController.java
@@ -1,4 +1,47 @@
 package edu.toronto.csc207.restaurantsolution.gui.newAccount;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class createAccountController {
+
+    @FXML
+    private JFXTextField name;
+
+    @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXCheckBox serverBox;
+
+    @FXML
+    private JFXCheckBox receiverBox;
+
+    @FXML
+    private JFXCheckBox chefBox;
+
+    @FXML
+    private JFXCheckBox cashierBox;
+
+    @FXML
+    private JFXCheckBox managerBox;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private JFXPasswordField confirmPassword;
+
+    @FXML
+    private JFXButton confirmButton;
+
+    @FXML
+    void confirmAction(ActionEvent event) {
+
+    }
+
 }
